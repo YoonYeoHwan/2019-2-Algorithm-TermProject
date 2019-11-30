e504234866fa0cfc507b670cf600c5d4026a433c
Gihyeon Yang <dexterastin@gmail.com>
2019-12-01 00:24:29 +0900

    feat: add format_string func & change setMap method

--- map.cpp
+++ map.cpp
@@ -1,99 +1,37 @@
 #include <fstream>
 #include <iostream>
+#include "string.h"
 #include "map.h"
 
 using namespace std;
 
+std::string format_string(const std::string fmt, ...) {
+    int size = ((int) fmt.size()) * 2;
+    std::string buffer;
+    va_list ap;
+    while (1) {
+        buffer.resize(size);
+        va_start(ap, fmt);
+        int n = vsnprintf((char *) buffer.data(), size, fmt.c_str(), ap);
+        va_end(ap);
+        if (n > -1 && n < size) {
+            buffer.resize(n);
+            return buffer;
+        }
+        if (n > -1) size = n + 1; else size *= 2;
+    }
+    return buffer;
+}
+
 void mapArray::set_map(int map_arr[][10], int level, int &a, int &b, int &goalCount) {
     ifstream input;
-    if (level == 1) {
-        input.open("./mapFile/level1.txt");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                input >> map_arr[i][j];
-            }
+    std::string filename = format_string("./mapFile/level%d.txt", level);
+    input.open(filename);
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            input >> map_arr[i][j];
         }
-        input >> goalCount >> a >> b;
-
-    } else if (level == 2) {
-        input.open("./mapFile/level2.txt");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                input >> map_arr[i][j];
-            }
-        }
-        input >> goalCount >> a >> b;
-    } else if (level == 3) {
-        input.open("./mapFile/level3.txt");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                input >> map_arr[i][j];
-            }
-        }
-        input >> goalCount >> a >> b;
     }
-    if (level == 4) {
-        input.open("./mapFile/level4.txt");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                input >> map_arr[i][j];
-            }
-        }
-        input >> goalCount >> a >> b;
-    }
-    if (level == 5) {
-        input.open("./mapFile/level5.txt");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                input >> map_arr[i][j];
-            }
-        }
-        input >> goalCount >> a >> b;
-    }
-    if (level == 6) {
-        input.open("./mapFile/level6.txt");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                input >> map_arr[i][j];
-            }
-        }
-        input >> goalCount >> a >> b;
-    }
-    if (level == 7) {
-        input.open("./mapFile/level7.txt");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                input >> map_arr[i][j];
-            }
-        }
-        input >> goalCount >> a >> b;
-    }
-    if (level == 8) {
-        input.open("./mapFile/level8.txt");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                input >> map_arr[i][j];
-            }
-        }
-        input >> goalCount >> a >> b;
-    }
-    if (level == 9) {
-        input.open("./mapFile/level9.txt");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                input >> map_arr[i][j];
-            }
-        }
-        input >> goalCount >> a >> b;
-    }
-    if (level == 10) {
-        input.open("./mapFile/level10.txt");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                input >> map_arr[i][j];
-            }
-        }
-        input >> goalCount >> a >> b;
-    }
+    input >> goalCount >> a >> b;
 }
 
